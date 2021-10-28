94e2b675db6d8303ce46780a64fabcd6f84453ed
ShaquilleClarke <github email address>
2021-10-27 21:46:03 -0400

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-//import DStorage from '../abis/DStorage.json'
+import DStorage from '../abis/DStorage.json'
 import React, { Component } from 'react';
 import Navbar from './Navbar'
 import Main from './Main'
 import Web3 from 'web3';
 import './App.css';
 
+const log = console.log;
+
 //Declare IPFS
+const ipfsClient = require('ipfs-http-client'),
+ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
 
 class App extends Component {
 
@@ -15,57 +19,112 @@ class App extends Component {
   }
 
   async loadWeb3() {
-    //Setting up Web3
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
   }
 
   async loadBlockchainData() {
-    //Declare Web3
-
-    //Load account
-
-    //Network ID
-
-    //IF got connection, get data from contracts
-      //Assign contract
-
-      //Get files amount
-
-      //Load files&sort by the newest
-
-    //Else
-      //alert Error
-
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    // Network ID
+    const networkId = await web3.eth.net.getId()
+    const networkData = DStorage.networks[networkId]
+    if(networkData) {
+      // Assign contract
+      const dstorage = new web3.eth.Contract(DStorage.abi, networkData.address)
+      this.setState({ dstorage })
+      // Get files amount
+      const filesCount = await dstorage.methods.fileCount().call()
+      this.setState({ filesCount })
+      // Load files&sort by the newest
+      for (var i = filesCount; i >= 1; i--) {
+        const file = await dstorage.methods.files(i).call()
+        this.setState({
+          files: [...this.state.files, file]
+        })
+      }
+    } else {
+      window.alert('DStorage contract not deployed to detected network.')
+    }
   }
 
   // Get file from user
   captureFile = event => {
-  }
+    event.preventDefault()
 
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
 
-  //Upload File
-  uploadFile = description => {
-
-    //Add file to the IPFS
-
-      //Check If error
-        //Return error
-
-      //Set state to loading
-
-      //Assign value for the file without extension
-
-      //Call smart contract uploadFile function 
-
-  }
-
-  //Set states
-  constructor(props) {
-    super(props)
-    this.state = {
+    log(file)
+    reader.readAsArrayBuffer(file)
+    
+    reader.onloadend = () => {
+      this.setState({
+        buffer: Buffer(reader.result),
+        type: file.type,
+        name: file.name
+      })
+      log('buffer', this.state.buffer)
     }
-
-    //Bind functions
   }
+
+  uploadFile = description => {
+    log("Submitting file to IPFS...")
+    // Add file to the IPFS
+
+    //We need an alert here!
+    if (this.state.buffer === undefined) {
+      window.alert('Please choose a file');
+      return;
+    } else {
+
+      ipfs.add(this.state.buffer, (error, result) => {
+        log('IPFS result', result.size)
+        // if(error) {
+        //   console.error(error)
+        //   return
+        // }
+  
+        this.setState({ loading: true })
+        // Assign value for the file without extension
+        if(this.state.type === ''){
+          this.setState({type: 'none'})
+        }
+        this.state.dstorage.methods.uploadFile(result[0].hash, result[0].size, this.state.type, this.state.name, description).send({ from: this.state.account }).on('transactionHash', (hash) => {
+          this.setState({
+           loading: false,
+           type: null,
+           name: null
+         })
+         window.location.reload()
+        }).on('error', (e) =>{
+          window.alert('Error')
+          this.setState({loading: false})
+        })
+      })
+    }
+  }
+
+
+  state = {
+    account: '',
+    dstorage: null,
+    files: [],
+    loading: false,
+    type: null,
+    name: null
+  }
+  
 
   render() {
     return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,19 +5,42 @@ import box from '../box.png'
 class Navbar extends Component {
 
   render() {
+    const { account } = this.props;
     return (
       <nav className="navbar navbar-dark bg-dark p-0 text-monospace">
+        {/* replace with link to your linkedin */}
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
+          href="https://github.com/ShaquilleClarke"
           target="_blank"
           rel="noopener noreferrer"
         >
+         
           <img src={box} width="30" height="30" className="align-top" alt="" />
-          D$t0r@g3
+          B0d3g@
         </a>
         <ul className="navbar-nav px-3">
-          <b className='text-white'>{'0x0'}</b>
+          <li>
+              <small id="account">
+                <a target="_blank"
+                  alt=""
+                  className="text-white"
+                  rel="noopener noreferrer"
+                  href={"https://etherscan.io/address/" + account}>
+                  {account ? account.substring(0,6) : '0x0'}...{account ? account.substring(38,42) : '0x0'}  
+                </a>
+              </small>
+              { account
+                ? <img
+                    alt=""
+                    className='ml-2'
+                    width='30'
+                    height='30'
+                    src={`data:image/png;base64,${new Identicon(account, 30).toString()}`}
+                  />
+                : <span></span>
+              }
+            </li>
         </ul>
       </nav>
     );
